e0bf230cebabcc7268681f2137f915f3aa441417
jonak1064 <124136656+jonak1064@users.noreply.github.com>
2024-04-21 23:47:19 +0600

    data

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,58 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'flutter ',
+        amount: 99.9,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: ' cinema  ',
+        amount: 99.9,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('kk'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('the chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
